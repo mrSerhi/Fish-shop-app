788bc9ff4bdc7f3b2a2b77f4e3e99760cae22a69
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-04-10 18:20:34 +0300

    changes name the root file of app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Header, Order, Inventory
 import StorePicker from "./components/StorePicker";
 import NotFound from "./components/Pages/NotFound/NotFound";
-import ShopDescboard from "./components/ShopDescboard/ShopDescboard";
+import ShopDashboard from "./components/ShopDashboard/ShopDashboard";
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={StorePicker} />
-          <Route path="/store/:store_id" component={ShopDescboard} />
+          <Route path="/store/:store_id" component={ShopDashboard} />
           <Route component={NotFound} />
         </Switch>
       </Router>
