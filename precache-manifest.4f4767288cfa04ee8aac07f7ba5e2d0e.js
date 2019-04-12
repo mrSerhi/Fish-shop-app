16cafa56daa73b14cce0b6d9b9113949fd35f244
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-04-12 11:37:57 +0300

    Updates

---
+++ precache-manifest.4f4767288cfa04ee8aac07f7ba5e2d0e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/Fish-shop-app/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "7611d6f0a643b594cdfc",
+    "url": "/Fish-shop-app/static/css/main.230df702.chunk.css"
+  },
+  {
+    "revision": "7b73cc18b26286a7c8c2",
+    "url": "/Fish-shop-app/static/js/runtime~main.9b6b74e3.js"
+  },
+  {
+    "revision": "37ba4fffd1ddcf1e95a3",
+    "url": "/Fish-shop-app/static/js/2.7218e369.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/Fish-shop-app/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/Fish-shop-app/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/Fish-shop-app/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "7611d6f0a643b594cdfc",
+    "url": "/Fish-shop-app/static/js/main.7c6766eb.chunk.js"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/Fish-shop-app/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/Fish-shop-app/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/Fish-shop-app/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/Fish-shop-app/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/Fish-shop-app/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "d1bf5509c70b50d14b521ea0dac3132d",
+    "url": "/Fish-shop-app/index.html"
+  }
+];
